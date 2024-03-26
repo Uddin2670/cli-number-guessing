03376147c06091dde2ed3a95790c384761d75972
HAMZA UDDIN <160138559+Uddin2670@users.noreply.github.com>
2024-03-26 16:12:30 -0700

    Add files via upload
    created a cli number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessingNumber",
+        type: "number",
+        message: "please guess the number between 1-6:",
+    },
+]);
+if (answers.UserGuessingNumber === randomNumber) {
+    console.log("congtratulation ! you guessed right number.");
+}
+else {
+    console.log("you loss you guessed wrong number");
+}
+console.log("THE END");
